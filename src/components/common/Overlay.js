f95ee161c51be41750f48411169722b584a7ba03
Suleng18 <90331457+suleng18@users.noreply.github.com>
2023-02-06 21:57:54 +0700

    component search

---
+++ src/components/common/Overlay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Overlay = () => {
+  return <div className="fixed inset-0 z-40 bg-black opacity-0 overlay bg-opacity-10"></div>;
+};
+
+export default Overlay;
+// 386
